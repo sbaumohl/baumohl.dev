db5cebfcb9c460e1ee9815cf4bee00caf79fda9d
Sam Baumohl <28550422+sbaumohl@users.noreply.github.com>
2025-10-02 11:32:40 -0500

    Senior update (#23)
    * let's clean some things
    
    * so that's the issue
    
    * done
    
    * rss
    
    * upgrade tailwind
    
    * audit

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,7 @@ const blog = defineCollection({
     // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
+    useOutline: z.boolean().default(true),
   }),
 });
 
